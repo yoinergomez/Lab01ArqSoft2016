a086c7a77eac537ba5f772b5d5ca31839a1e27ea
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-03-24 20:56:04 -0500

    Creacion de la clase validadora

---
+++ src/java/com/udea/edu/laboratorio1/util/ValidadorEntrada.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.edu.laboratorio1.util;
+
+/**
+ *
+ * @author esteban
+ */
+public class ValidadorEntrada {
+    
+}
